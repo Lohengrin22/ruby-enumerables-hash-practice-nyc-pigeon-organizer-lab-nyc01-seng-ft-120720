44e04bbb94b912a48727909cef03707a3fd67502
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-03 17:17:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-final = data.each_with_object({}) do |(key, value), result|
+data.each_with_object({}) do |(key, value), result|
   value.each do |inner_k, names|
     names.each do |name|
       if !result[name]
